bba5118a9402688b5a36d046d702913946d07a38
Nicolas <nickeyventura@gmail.com>
2026-05-31 19:35:44 -0300

    Posição janelas

--- CadastroUsuarios.Designer.cs
+++ CadastroUsuarios.Designer.cs
@@ -119,6 +119,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "CadastroUsuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CadastroUsuarios";
             this.ResumeLayout(false);
             this.PerformLayout();
--- TelaEntrar.Designer.cs
+++ TelaEntrar.Designer.cs
@@ -94,6 +94,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "TelaEntrar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TelaEntrar";
             this.ResumeLayout(false);
             this.PerformLayout();
